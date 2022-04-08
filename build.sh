b010219ca8dedbd3eaf0f562bc970e1cb293fe3a
yjr4df0708 <95735486+yjr4df0708@users.noreply.github.com>
2022-04-07 18:28:54 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# There is definitely a better way to do this.
+if [[ -z "$1" ]]; then
+	lex="interpreter.l";
+else
+	lex="$1";
+fi
+if [[ -z "$2" ]]; then
+	yacc="interpreter.y";
+else
+	yacc="$2";
+fi
+if [[ -z "$3" ]]; then
+	out="interpreter";
+else
+	out="$3";
+fi
+flex $lex
+bison $yacc
+gcc -o $out lex.yy.c
